59a7c36bb894d773466dc6b3f2f66855131333f5
Davy Chen <davy_chen@epam.com>
2020-11-13 17:05:58 +0800

    kick off

---
+++ pages/posts/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+export default function Post() {
+  return (
+    <Layout>
+      <h1>First Post</h1>
+    </Layout>
+  )
+}
